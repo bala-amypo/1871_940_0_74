39eb2840bde99eb8cb3fd355ad48ce6c27460709
bala-amypo <bala@amypo.in>
2025-12-17 06:06:28 +0000

    User commit

--- src/main/java/com/example/demo/entity/Student.java
+++ src/main/java/com/example/demo/entity/Student.java
@@ -2,37 +2,22 @@ package com.example.demo.entity;
 
 import jakarta.persistence.*;
 
-@entity
+@Entity
 @Table(name = "users")
+public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-public class Student {
-    private int id;
     private String name;
-    private String email;
-    
-    public int getId() {
-        return id;
-    }
-    public void setId(int id) {
-        this.id = id;
-    }
-    public Student(int id, String name, String email) {
-        this.id = id;
-        this.name = name;
-        this.email = email;
-    }
-    public String getName() {
-        return name;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
-    public String getEmail() {
-        return email;
-    }
-    public void setEmail(String email) {
-        this.email = email;
-    }
 
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+
+    private String role;
+
+    // getters & setters
 }
