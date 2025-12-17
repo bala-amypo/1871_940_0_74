ed9a09276c8baebb186ff378c90a22dc2d3807e2
bala-amypo <bala@amypo.in>
2025-12-17 10:04:26 +0000

    User commit

--- src/main/java/com/example/demo/service/StudentService.java
+++ src/main/java/com/example/demo/service/StudentService.java
@@ -7,6 +7,6 @@ import com.example.demo.entity.Student;
 @Service
 public interface StudentService {
     
-    Student saveStudent(Student student);// Creating object for saving student details and class
+    Student saveStudent(Student student);
     
 }
